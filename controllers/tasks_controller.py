87b0c72e15e6465d1b39040eaae2eef7f20cfa31
John <ashton.basile@gmail.com>
2023-07-18 13:45:41 +0100

    I hooked up the templates (views)

--- controllers/tasks_controller.py
+++ controllers/tasks_controller.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 tasks_blueprint = Blueprint("tasks", __name__)
 
 @tasks_blueprint.route("/tasks")
 def index():
-	return "<h1>Tasks be here</h1>"
+	return render_template("index.html", title="My Task List")
